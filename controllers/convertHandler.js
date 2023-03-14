b074c8a1e932a820a0e88e046f6ecfebe2eda77b
Oluwayimika Michael Oladipupo <oladipupomichael9@gmail.com>
2023-03-14 11:52:01 +0100

    chore: unit tests

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,81 @@
+const numberStringSplitter = (input) => {
+  let number = input.match(/[.\d\/]+/g) || ["1"];
+  let string = input.match(/[a-zA-Z+]/g)[0];
+  return [number[0], string];
+};
+
+const checkDiv = (fraction) => {
+  let nums = fraction.split("/");
+  if (nums.length > 2) {
+    return false;
+  }
+  return nums;
+};
+
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
+  this.getNum = function (input) {
+    let result = numberStringSplitter(input)[0];
+    let nums = checkDiv(result);
+
+    if (!nums) return undefined;
+
+    let num1 = nums[0];
+    let num2 = nums[1] || "1";
+
+    result = parseFloat(num1) / parseFloat(num2);
+
+    if (isNaN(num1) || isNaN(num2)) return undefined;
+
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
+  this.getUnit = function (input) {
+    let result = numberStringSplitter(input)[1].toLowerCase();
+    switch (result) {
+      case "km":
+        return "km";
+      case "gal":
+        return "gal";
+      case "lbs":
+        return "lbs";
+      case "mi":
+        return "mi";
+      case "l":
+        return "L";
+      case "kg":
+        return "kg";
+      default:
+        return undefined;
+    }
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.getReturnUnit = function (initUnit) {
+    let result;
+
+    return result;
+  };
+
+  this.spellOutUnit = function (unit) {
+    let result;
+
+    return result;
+  };
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     let result;
-    
+
     return result;
   };
-  
 }
 
 module.exports = ConvertHandler;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,47 @@
-const chai = require('chai');
+const chai = require("chai");
 let assert = chai.assert;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 let convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
-
+suite("Unit Tests", function () {
+  suite("Convert Handler", () => {
+    // #1
+    test("#isWholeNum, #isNotWholeNum", () => {
+      assert.isTrue(convertHandler.getNum("50mi") % 1 == 0, "50 is a number");
+      assert.isNotTrue(
+        convertHandler.getNum("3.1mi") % 1 == 0,
+        "3.1 is not a whole number"
+      );
+    });
+    // #2
+    test("#isDecimal, #isNotDecimal", () => {
+      assert.isTrue(
+        convertHandler.getNum("4.8mi") % 1 !== 0,
+        "4.8 is a decimal"
+      );
+      assert.isNotTrue(
+        convertHandler.getNum("20kg") % 1 !== 0,
+        "20 is not a decimal"
+      );
+    });
+    // #3
+    test("#isFraction", () => {
+      assert.isTrue(
+        convertHandler.getNum("3/5mi") == 3 / 5,
+        "3/5 should equal 3/5"
+      );
+      assert.isTrue(
+        convertHandler.getNum("3/6mi") == 1 / 2,
+        "3/6 should equal 1/2"
+      );
+    });
+    // #4
+    test("#isDecimalFraction", () => {
+      assert.isTrue(
+        convertHandler.getNum("1.2/3kg") == 1.2 / 3,
+        "Should be equal"
+      );
+    });
+  });
 });
